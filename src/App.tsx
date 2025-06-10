aa556a7c9364bce3ca508c30d17b723a15933977
Trillium Smith <trillium@trilliumsmith.com>
2025-06-09 18:00:42 -0700

    feat: use script-data.json first if values exist

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import data from "./data.json";
+import scriptData from "./script-data.json";
+import fallbackData from "./data.json";
 import "./App.css";
 
 import Category from "./components/Category/Category";
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
+
+import type { CVData } from "./types";
+
+function isValidData(data: any): data is CVData {
+  return !!(
+    data &&
+    typeof data === "object" &&
+    "header" in data &&
+    "workExperience" in data &&
+    "profile" in data &&
+    "technical" in data &&
+    "languages" in data &&
+    "education" in data
+  );
+}
+
+const data: CVData = isValidData(scriptData) ? scriptData : fallbackData;
 
 function App() {
   return (
@@ -19,7 +37,9 @@ function App() {
           }}
         >
           <Category data={data.workExperience} />
-          {data?.projects?.length > 0 && <Category data={data.projects} />}
+          {Array.isArray(data.projects) && data.projects.length > 0 && (
+            <Category data={data.projects} />
+          )}
         </div>
         <div className="app__body-right">
           <Profile data={data} />
