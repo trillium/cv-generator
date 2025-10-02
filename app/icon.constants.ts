5ece253cd0423cee6d6c832a3e15e9107d3c1af0
Trillium Smith <trillium@trilliumsmith.com>
2025-10-02 15:40:56 -0700

    fix(lint): move non-component exports to icon.constants.ts for Fast Refresh

---
+++ app/icon.constants.ts
@@ -0,0 +1,5 @@
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
--- app/icon.tsx
+++ app/icon.tsx
@@ -1,26 +1,10 @@
 import { ImageResponse } from "next/og";
+import { size, contentType } from "./icon.constants";
 
-// Image metadata
-export const size = {
-  width: 32,
-  height: 32,
-};
-export const contentType = "image/png";
-
-// Image generation
 export default function Icon() {
-  return new ImageResponse(
-    (
-      // ImageResponse JSX element
-      <InlineStyles />
-    ),
-    // ImageResponse options
-    {
-      // For convenience, we can re-use the exported icons size metadata
-      // config to also set the ImageResponse's width and height.
-      ...size,
-    },
-  );
+  return new ImageResponse(<InlineStyles />, {
+    ...size,
+  });
 }
 
 // InlineStyles version:
