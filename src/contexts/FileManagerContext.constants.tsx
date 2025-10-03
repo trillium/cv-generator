6cd27472aba016fc6fa73364ec9293c1a1d7d40f
Trillium Smith <trillium@trilliumsmith.com>
2025-10-02 18:58:11 -0700

    fix(FileManagerContext): add missing createContext import

--- src/contexts/FileManagerContext.constants.tsx
+++ src/contexts/FileManagerContext.constants.tsx
@@ -1,4 +1,5 @@
 // Utility/context constants for FileManagerContext
+import { createContext } from "react";
 import { FileManagerContextType } from "../types/fileManager";
 
 const FileManagerContext = createContext<FileManagerContextType | undefined>(
