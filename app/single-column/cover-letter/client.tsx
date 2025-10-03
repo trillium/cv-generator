15686ef710fb2d6fe7d83bce78bc8e5649691fad
Trillium Smith <trillium@trilliumsmith.com>
2025-10-02 18:53:08 -0700

    refactor(single-column/cover-letter): update import location of useFileManager hook

--- app/single-column/cover-letter/client.tsx
+++ app/single-column/cover-letter/client.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import SingleColumnCoverLetter from "../../../src/components/Resume/single-column/cover-letter";
-import { useFileManager } from "../../../src/contexts/FileManagerContext";
+import { useFileManager } from "../../../src/contexts/FileManagerContext.hook";
 import { decodeFilePathFromUrl } from "../../../src/utils/urlSafeEncoding";
 import type { CVData } from "../../../src/types";
 
