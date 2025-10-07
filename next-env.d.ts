0be7a437eca7b6a08ee8c59f366e1cc7c2c35a8d
Trillium Smith <trillium@trilliumsmith.com>
2025-10-07 10:40:29 -0700

    chore(infra): update Next.js env types and dev server port

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-// eslint-disable-next-line
+/// <reference types="next/navigation-types/compat/navigation" />
 /// <reference path="./.next/types/routes.d.ts" />
 
 // NOTE: This file should not be edited
