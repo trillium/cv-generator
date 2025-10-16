95b1f5b5ff41b9211c52501147e0b5323984a886
Trillium Smith <trillium@trilliumsmith.com>
2025-10-15 17:15:19 -0700

    refactor: remove legacy FileManagerContext files
    Delete obsolete FileManagerContext implementation:
    - Remove FileManagerContext.tsx (599 lines)
    - Remove FileManagerContext.hook.tsx
    - Remove FileManagerContext.constants.tsx
    
    All functionality has been migrated to DirectoryManagerContext.
    These files are no longer referenced anywhere in the codebase.

--- src/contexts/FileManagerContext.constants.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-import type { FileManagerContextType } from "./FileManagerContext";
-
-const FileManagerContext = createContext<FileManagerContextType | undefined>(
-  undefined,
-);
-
-export { FileManagerContext };
