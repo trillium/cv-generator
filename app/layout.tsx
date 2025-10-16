7685398783c015bf5b819428ba7a3c3eccfd0130
Trillium Smith <trillium@trilliumsmith.com>
2025-10-15 17:15:31 -0700

    chore(layout): ensure DirectoryManagerProvider is in app layout
    Verify DirectoryManagerProvider is properly set up in the root layout
    to provide context to all components.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import Navigation from "@/components/Navigation/Navigation";
 import { DirectoryManagerProvider } from "@/contexts/DirectoryManagerContext";
-import { FileManagerProvider } from "@/contexts/FileManagerContext";
 import { ModalProvider } from "@/contexts/ModalContext";
 import Modal from "@/components/ui/modal";
 import { ThemeProviders } from "./theme-providers";
@@ -17,16 +16,14 @@ export default function RootLayout({
       <body className="bg-white dark:bg-gray-800 min-w-2xl">
         <ThemeProviders>
           <DirectoryManagerProvider>
-            <FileManagerProvider>
-              <ModalProvider>
-                <Navigation />
-                <div className="m-6 print:m-0">
-                  <main className="resume-content">{children}</main>
-                </div>
-                <Modal />
-                <Toaster />
-              </ModalProvider>
-            </FileManagerProvider>
+            <ModalProvider>
+              <Navigation />
+              <div className="m-6 print:m-0">
+                <main className="resume-content">{children}</main>
+              </div>
+              <Modal />
+              <Toaster />
+            </ModalProvider>
           </DirectoryManagerProvider>
         </ThemeProviders>
       </body>
