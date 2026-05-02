9f7b876c56493579b6d4056653548f872291609b
Trillium Smith <trillium@trilliumsmith.com>
2026-05-01 22:20:55 -0700

    fix(spacing): add top margin and bottom separator spacing to section titles

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -2,9 +2,9 @@ import Separator from '@/components/Separator/Separator'
 
 const Title = ({ text }: { text: string }) => {
   return (
-    <div className="w-full">
+    <div className="w-full mt-3">
       <div className="text-primary-500 rounded text-base font-bold">{text}</div>
-      <Separator className="" />
+      <Separator className="mb-2" />
     </div>
   )
 }
