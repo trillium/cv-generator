b021e5532b36cab84fef82465d495598bad07a18
Trillium Smith <trillium@trilliumsmith.com>
2026-05-20 20:55:53 -0700

    chore: configure turbopack root

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const isDev = process.env.NODE_ENV !== 'production'
 
 const nextConfig = {
   distDir: isDev ? '.next-dev' : '.next',
+  turbopack: {
+    root: '.',
+  },
 }
 
 export default nextConfig
